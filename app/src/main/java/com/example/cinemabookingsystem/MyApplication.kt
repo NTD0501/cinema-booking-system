ecc434a1af8cf5a41ea9ba36d3e528d134fa0eb8
Code2025@vn <khoahoctienganh2021@gmail.com>
2025-03-20 02:27:21 +0700

    add authenticate screens

---
+++ app/src/main/java/com/example/cinemabookingsystem/MyApplication.kt
@@ -0,0 +1,44 @@
+package com.example.cinemabookingsystem
+
+import android.app.Application
+import android.content.Context
+import com.example.cinemabookingsystem.prefs.DataStoreManager
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DataStoreManager.init(applicationContext)
+        FirebaseApp.initializeApp(this)
+    }
+
+    fun getFoodDatabaseReference(): DatabaseReference {
+        return FirebaseDatabase.getInstance(FIREBASE_URL).getReference("/food")
+    }
+
+    fun getCategoryDatabaseReference(): DatabaseReference {
+        return FirebaseDatabase.getInstance(FIREBASE_URL).getReference("/category")
+    }
+
+    fun getMovieDatabaseReference(): DatabaseReference {
+        return FirebaseDatabase.getInstance(FIREBASE_URL).getReference("/movie")
+    }
+
+    fun getBookingDatabaseReference(): DatabaseReference {
+        return FirebaseDatabase.getInstance(FIREBASE_URL).getReference("/booking")
+    }
+
+    fun getQuantityDatabaseReference(foodId: Long): DatabaseReference {
+        return FirebaseDatabase.getInstance().getReference("/food/$foodId/quantity")
+    }
+
+    companion object {
+        private const val FIREBASE_URL = "https://cinema-c68f6-default-rtdb.firebaseio.com"
+        operator fun get(context: Context?): MyApplication {
+            return context!!.applicationContext as MyApplication
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cinemabookingsystem/listener/IGetDateListener.kt
@@ -0,0 +1,5 @@
+package com.example.cinemabookingsystem.listener
+
+interface IGetDateListener {
+    fun getDate(date: String?)
+}
